9df6ffe06ca324c1137467be8345169ef335da8e
Luis H. Hernandez <guicholh@gmail.com>
2022-07-29 11:56:19 -0500

    added content negotiation, invoice model, invoice dto, invoice controller, invoice service, user model, and user service.

---
+++ src/main/java/com/lucodes/myfancypdfinvoices/springboot/service/UserService.java
@@ -0,0 +1,15 @@
+package com.lucodes.myfancypdfinvoices.springboot.service;
+
+import com.lucodes.myfancypdfinvoices.springboot.model.User;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class UserService {
+
+    public User findById(String id) {
+        String randomName = UUID.randomUUID().toString();
+        return new User(id, randomName);
+    }
+}
---
+++ src/main/java/com/lucodes/myfancypdfinvoices/springboot/web/InvoicesController.java
@@ -0,0 +1,32 @@
+package com.lucodes.myfancypdfinvoices.springboot.web;
+
+import com.lucodes.myfancypdfinvoices.springboot.dto.InvoiceDto;
+import com.lucodes.myfancypdfinvoices.springboot.model.Invoice;
+import com.lucodes.myfancypdfinvoices.springboot.service.InvoiceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class InvoicesController {
+
+    private final InvoiceService invoiceService;
+
+    public InvoicesController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/invoices")
+    public List<Invoice> invoices() {
+        return invoiceService.findAll();
+    }
+
+    @PostMapping("/invoices")
+    public Invoice createInvoice(@Valid @RequestBody InvoiceDto invoiceDto) {
+        return invoiceService.create(invoiceDto.getUserId(), invoiceDto.getAmount());
+    }
+}
