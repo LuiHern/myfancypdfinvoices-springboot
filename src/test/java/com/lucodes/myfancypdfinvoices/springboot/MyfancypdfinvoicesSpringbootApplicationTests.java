f127288e43f64881f0be53a1912a0ac530dc4906
Luis H. Hernandez <guicholh@gmail.com>
2022-07-29 10:58:18 -0500

    initial commit

---
+++ src/test/java/com/lucodes/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucodes.myfancypdfinvoices.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfancypdfinvoicesSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
